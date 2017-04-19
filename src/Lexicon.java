f84e45b422c069c2434ebd617fe462a26da457f9
Dmitry Zotov <Dmitriyzotov2009@yandex.ru>
2017-04-20 00:33:48 +0300

    count clauses

--- src/Lexicon.java
+++ src/Lexicon.java
@@ -2,6 +2,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.TreeMap;
@@ -9,59 +10,93 @@ import java.io.File;
 
 class Parser {
     private String fileName;
-    private String pattern = "[A-Za-z0-9,.()]+";
+    private String pattern;
     private String word;
-    private int val;
+    private int val = 1;
     private Path path;
     private Scanner scanner;
-    private Map<String , Integer> words;
+    private Map<String, Integer> words;
+    private ArrayList<String> elements;
+    private File file;
+    private FileWriter fw;
 
-    public Parser(String fileName) throws IOException {
-        fileName = fileName;
-        val = 1;
+    public Parser(String fileN) throws IOException {
+        this.fileName = fileN;
         words = new TreeMap<>();
         path = Paths.get(fileName);
         scanner = new Scanner(path);
-        // если юзать TreeMap сложность будет О(log(n))
-        // если юзать HashMap среднем О(1) в худшем О(n),
-        while (scanner.hasNext()){
-            if(scanner.hasNext(pattern)){
-                word = scanner.next();
-                word = word.replaceAll("[^A-Za-z0-9]", "");
-                if (words.containsKey(word)){
-                    int value = words.get(word);
-                    value++;
-                    words.put(word,value);
-                }
-                else words.put(word,val);
-            }
-            else scanner.next();
-        }
-        writeResult();
+        elements = new ArrayList<>();
+        pattern = ("[\\w.,:;!?»«()]+");//???»«
+        scanText(pattern);
+        file = new File("lexicon.txt");
+        fw = new FileWriter(file);
+
     }
-    private void writeResult(){
-        File file = new File("lexicon.txt");
-        FileWriter fr = null;
+
+    private void scanText(String p) {
+        pattern = p;
+        while (scanner.hasNext()) {
+            if (scanner.hasNext(pattern)) {
+                word = scanner.next();
+                elements.add(word);
+            } else scanner.next();
+        }
+    }
+
+    public void wordsStatistic() {
+        for (String w : elements) {
+            w = w.replaceAll("[^A-Za-z0-9]", "");
+            if (words.containsKey(w)) {
+                int value = words.get(w);
+                value++;
+                words.put(w, value);
+            } else words.put(w, val);
+        }
         try {
-            fr = new FileWriter(file);
-            for(Map.Entry<String,Integer> entry : words.entrySet()) {
+            //fw = new FileWriter(file,true);
+            for (Map.Entry<String, Integer> entry : words.entrySet()) {
                 String key = entry.getKey();
                 Integer value = entry.getValue();
-                fr.write(key + " - " + value+"\r\n");
+                fw.write(key + " - " + value + "\r\n");
             }
         } catch (IOException e) {
             e.printStackTrace();
-        }finally{
+        } finally {
             try {
-                fr.close();
+                fw.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void clausesCounter() {
+        int clauses = 0;
+        for (String w : elements) {
+            if (w.contains(".") || w.contains("?") ||
+                    w.contains("!") || w.contains("...")) {
+                clauses++;
+            }
+        }
+        try {
+            fw = new FileWriter(file, true);//????
+            fw.write("\r\nClauses count = " + clauses);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fw.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
     }
 }
+
 public class Lexicon {
     public static void main(String[] args) throws IOException {
-        Parser par = new Parser(args[0]);
+        Parser par = new Parser("input.txt");
+        par.wordsStatistic();
+        par.clausesCounter();
     }
 }
